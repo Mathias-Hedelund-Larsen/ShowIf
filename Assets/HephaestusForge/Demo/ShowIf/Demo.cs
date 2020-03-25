7afc104fd9375668efadf7d3e90c0455ba59c094
MathDodo <mathias.h.larsen@jp.dk>
2020-03-25 15:29:44 +0100

    Setup of the attribute

---
+++ Assets/HephaestusForge/Demo/ShowIf/Demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HephaestusForge.ShowIf
+{
+    public class Demo : MonoBehaviour
+    {
+        [SerializeField, ShowIf]
+        private int _tVal;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/HephaestusForge/ShowIf/Scripts/Other/Attributes/ShowIf.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+
+namespace HephaestusForge.ShowIf
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ShowIf : PropertyAttribute
+    {
+        [Flags]
+        public enum Comparison
+        {
+            Equals = 1 << 0,
+            GreaterThan = 1 << 1,
+            LessThan = 1 << 2,
+            NotEqualTo = 1 << 3
+        }
+
+        public string FieldName { get; }
+
+        private IComparable _comparableValue;
+
+        internal Func<IComparable, bool>[] _Compare;
+
+        /// <summary>
+        /// The field used with this constructor needs to be a bool, and will be shown if true
+        /// </summary>
+        public ShowIf(Comparison comparison = Comparison.Equals)
+        {
+
+        }
+
+        /// <summary>
+        /// The field used with this constructor will have its value checked, and will be shown if the value is equal
+        /// </summary>
+        /// <param name="value">The comparable value</param>
+        public ShowIf(IComparable value)
+        {
+            _comparableValue = value;
+        }
+
+        /// <summary>
+        /// The field name needs to refer to a bool field, and if the value is true this field will be shown
+        /// </summary>
+        /// <param name="fieldName">The name of the field</param>
+        public ShowIf(string fieldName)
+        {
+            FieldName = fieldName;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName">The name of the field</param>
+        /// <param name="value">The comparable value</param>
+        public ShowIf(string fieldName, IComparable value)
+        {
+            _comparableValue = value;
+            FieldName = fieldName;
+        }
+
+        private bool GreaterThan(IComparable comparable)
+        {
+            return comparable.CompareTo(_comparableValue) > 0;
+        }
+
+        private bool Equals(IComparable comparable)
+        {
+            return comparable.CompareTo(_comparableValue) == 0;
+        }
+
+        private bool GreaterLessThan(IComparable comparable)
+        {
+            return comparable.CompareTo(_comparableValue) < 0;
+        }
+    }
+}
